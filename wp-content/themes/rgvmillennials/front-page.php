de1afb4be9fbc7ac0d7da99de7139de135707317
Pablo De La Rosa <pablo@tgmedia.io>
2016-03-10 17:03:50 -0600

    Add basic page template structure and styles

--- wp-content/themes/rgvmillennials/front-page.php
+++ wp-content/themes/rgvmillennials/front-page.php
@@ -67,7 +67,7 @@
 								<h2>Reach Out</h2>
 							</header>
 							<div class="links">
-								<a class="button" href="#">Send Us A Message</a>
+								<a class="button" href="<?php echo site_url(); ?>/contact/">Send Us A Message</a>
 								<a class="button" href="https://www.facebook.com/rgvmillennials" target="_blank">Facebook</a>
 							</div>
 						</aside>
---
+++ wp-content/themes/rgvmillennials/page.php
@@ -0,0 +1,39 @@
+<?php get_header(); ?>
+
+	<?php if (is_user_logged_in()) { ?>
+
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+			<div class="page-wrap">
+
+				<div class="column row">
+
+					<header>
+						<h1>
+							<?php the_title(); ?>
+						</h1>
+					</header>
+
+					<div class="content">
+						<?php the_content(); ?>
+					</div>
+
+				</div>
+
+			</div>
+
+		<?php endwhile; else : ?>
+
+		<?php endif; ?>
+
+	<?php } else { ?>
+
+		<div class="landing">
+			<div class="column row">
+				<img class="rgvm-logo" src="<?php echo get_template_directory_uri(); ?>/images/rgvm-logo.svg" alt="RGV Millennials">
+			</div>
+		</div>
+
+	<?php } ?>
+
+<?php get_footer();
