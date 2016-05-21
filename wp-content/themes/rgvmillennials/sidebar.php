3bb00bd3eca1693ef33a89d2fffa6a5c55d9d1cd
Pablo De La Rosa <pablo@tgmedia.io>
2016-05-21 16:56:49 -0500

    Update sidebar with reachout links

--- wp-content/themes/rgvmillennials/sidebar.php
+++ wp-content/themes/rgvmillennials/sidebar.php
@@ -1,10 +1,13 @@
-<aside class="sidebar">
-	<hr class="hide-for-xxxlarge">
-	<h2 class="show-for-sr">2016 Summit</h2>
-	<a href="<?php bloginfo('url'); ?>/events/2016-summit">
-		<img src="<?php bloginfo('template_url'); ?>/images/2016-summit-flyer.jpg" alt="2016 RGV Millennials Summit Flyer Invitation">
-	</a>
-	<div class="text-center">
-		<a href="https://www.eventbrite.com/e/rgv-millennials-2016-summit-tickets-21846986926" class="button" target="_blank">Register To Attend</a>
+<div class="sidebar">
+	<div class="reach-out">
+		<aside>
+			<header>
+				<h2>Reach Out</h2>
+			</header>
+			<div class="links">
+				<a class="button" href="<?php echo site_url(); ?>/contact/">Send Us A Message</a>
+				<a class="button" href="https://www.facebook.com/rgvmillennials" target="_blank">Facebook</a>
+			</div>
+		</aside>
 	</div>
-</aside>
+</div>
