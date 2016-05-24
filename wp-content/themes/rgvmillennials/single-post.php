d0a3c384a2d52399f06cdaa7de1946d67b3e906b
Pablo De La Rosa <pablo@tgmedia.io>
2016-05-23 21:09:04 -0500

    Flexible field with nested gallery loop

--- wp-content/themes/rgvmillennials/single-post.php
+++ wp-content/themes/rgvmillennials/single-post.php
@@ -19,9 +19,46 @@
 							<li><a href="#" class="share-on-fb"><img class="icon-facebook" src="<?php echo get_template_directory_uri(); ?>/images/icon-facebook.svg" alt="Facebook"></a></li>
 						</ul>
 
-						<?php if (get_field('social_card_image')) { ?>
-							<img class="featured-image" src="<?php the_field('social_card_image'); ?>">
-						<?php } ?>
+
+						
+						<?php
+						// Post Feature
+
+						// check if the flexible content field has rows of data
+						if( have_rows('post_feature') ):
+
+						     // loop through the rows of data
+						    while ( have_rows('post_feature') ) : the_row();
+
+						        if( get_row_layout() == 'photo_gallery' ):
+
+						        	$images = get_sub_field('images');
+
+						        	if( $images ):
+
+						        		foreach( $images as $image ):
+
+						        			echo '<img src="' . $image['url'] . '" >';
+
+						        		endforeach;
+
+						        	endif;
+
+						        endif;
+
+						    endwhile;
+
+						else  :
+
+						    if (get_field('social_card_image')) { ?>
+
+								<img class="featured-image" src="<?php the_field('social_card_image'); ?>">
+						
+							<?php }
+
+						endif;
+
+						?>
 
 						<div class="content">
 							<?php the_content(); ?>
